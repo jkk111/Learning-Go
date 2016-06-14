0687483127529b6b2296a3393406b4d030f298b1
John Kelly <kellyj58@tcd.ie>
2016-06-14 23:09:40 +0100

    Added static file serving to webserver.go

--- webserver.go
+++ webserver.go
@@ -4,15 +4,23 @@ import(
   "fmt"
 )
 
+type httpHandler func(w http.ResponseWriter, r *http.Request)
+
 func main() {
-  http.HandleFunc("/", testHandler)
-  http.ListenAndServe(":8080", nil)
+  initWebServer();
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("hello world"))
+  if(r.URL.Path != "/") {
+    w.Write([]byte("404 Page Not Found"))
+  } else {
+    w.Write([]byte(fmt.Sprintf("Hello %s", r.Host)))
+  }
 }
 
-func serverReady() {
-  fmt.Println("Server ready")
+func initWebServer() {
+  http.Handle("/", http.FileServer(http.Dir("./static/")))
+  http.HandleFunc("*", testHandler)
+  http.ListenAndServe(":8080", nil)
+  fmt.Println("Server listening")
 }
